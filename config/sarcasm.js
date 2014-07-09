73224d762247bba19d4cb06d8641298fdff93e1e
4rg0n <argonthechecker@gmail.com>
2014-07-09 17:16:22 +0200

    initial commit

---
+++ config/sarcasm.js
@@ -0,0 +1,17 @@
+/*!
+ * sarcasm.js
+ * http://4rg0n.github.io/sarcasm.js
+ * MIT licensed
+ *
+ * Copyright (C) 2014 Dominic Rönicke
+ */
+
+//Initialize sarcasm.js
+SarcasmJs.initialize({
+
+    //name of the html tag (default)
+    selector: 'sarcasm',
+
+    //name of the CSS class(es) to set (default)
+    cssClass: 'animate bounce'
+});
